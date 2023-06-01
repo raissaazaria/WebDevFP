96ebb429950ab3635e18893c1b6e9a846ea12e5a
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2023-06-01 23:19:41 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://i.ibb.co/XsdmR2c/1.png",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://ibb.co/zx67VLY",
+      title: "6.6 SALE",
+      desc: "No.1 SKINCARE AT SAGARA.",
       bg: "f5fafd",
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://ibb.co/fvcVdZB",
+      title: "FREE SHIPPING",
+      desc: "DON'T COMPROMISE ON STYLE! FREE SHIPPING FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://i.ibb.co/cXFnLLV/3.png",
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      img: "https://i.pinimg.com/564x/43/06/4f/43064f0ef0f8a6c9a1d1fede8c540f0f.jpg",
+      title: "LEVEL UP YOUR OUTFIT",
+      desc: "TIMELESS BEAUTY: DISCOVER THE PERFECT NECKLACE FOR EVERY OCASSION",
       bg: "fbf0f4",
     },
   ];
@@ -25,25 +25,25 @@ export const sliderItems = [
   export const categories = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
+      img: "https://images.pexels.com/photos/5730956/pexels-photo-5730956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "MEN SHOES!",
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
+      img: "https://images.pexels.com/photos/3685530/pexels-photo-3685530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "SKINCARE",
     },
     {
       id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "LIGHT JACKETS",
+      img: "https://images.pexels.com/photos/2661256/pexels-photo-2661256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      title: "MAKEUP",
     },
   ];
 
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img:"https://www.lovebonito.com/cdn-cgi/image/width=2488,height=3456,quality=100,format=webp/https://public-images-for-media.s3.ap-southeast-1.amazonaws.com/media/catalog/product/l/n/ln0996-106-2_pgraqjkfkofp556h.jpg",
     },
     {
       id:2,
@@ -51,26 +51,26 @@ export const sliderItems = [
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img:"https://www.lovebonito.com/cdn-cgi/image/width=2488,height=3456,quality=100,format=webp/https://public-images-for-media.s3.ap-southeast-1.amazonaws.com/media/catalog/product/l/n/ln1058-031-2_ahh0he5hvrniycuc.jpg",
     },
     {
       id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      img:"https://www.newbalance.co.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/0/1/01-NEW-BALANCE-FFSSBNEW0-NEWMR530KA-Grey.jpg",
     },
     {
       id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      img:"https://www.newbalance.co.id/media/catalog/product/cache/144a401d4eb9f312744987a3cea154c3/0/1/01-NEW-BALANCE-ARAV6NEWA-NEWAWT11190P-Pink.jpg",
     },
     {
       id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      img:"https://id.coach.com/media/catalog/product/c/h/ch735_v5ebv_a0_wid_1500_hei_1500.jpg?optimize=low&bg-color=255,255,255&fit=bounds&height=960&width=960&canvas=960:960",
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img:"https://image-optimizer-id.production.sephora-asia.net/eyJlZGl0cyI6eyJyZXNpemUiOnt9fX0=/images/product_images/default_1_Product_3386460126571-Coach-Wild-Rose-Eau-De-Parfum-90ml_7374c622820993cbc18648baf26f678204607ebc_1661830936.png",
     },
     {
       id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      img:"https://image-optimizer-id.production.sephora-asia.net/eyJlZGl0cyI6eyJyZXNpemUiOnt9fX0=/images/product_images/default_1_Product_887167485488-Estee-Lauder-Advanced-Night-Repair-Synchronized-Multi-Recovery-Comp_5528a1b7bc297ce213a34da0dc5bec86c1e1b019_1617890216.png",
     },
   ]
