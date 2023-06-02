0066d6da137efa1808da8497bca5b8b85fbe869a
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-06-02 22:16:34 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-<script src="http://localhost:8097"></script>
 
-import Product from "./pages/Product";
+
+//import Product from "./pages/Product";
+import React from 'react';
 import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+//import ProductList from "./pages/ProductList";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Cart from "./pages/Cart";
+<script src="http://localhost:8097"></script>
 
 const App = () => {
   return <Home/>;
